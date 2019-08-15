cd08f7c6d7aa0a18a21335f377daf5d7418c3ae1
miftahunajat <miftahunajat@gmail.com>
2019-08-15 08:48:44 +0700

    change migration

--- db/migrate/20190815014334_change_jumlah_to_be_integer_in_list_pengeuarans.rb
+++ db/migrate/20190815014334_change_jumlah_to_be_integer_in_list_pengeuarans.rb
@@ -1,5 +1,6 @@
 class ChangeJumlahToBeIntegerInListPengeuarans < ActiveRecord::Migration[5.2]
   def change
-    change_column :list_pengeluarans, :jumlah, :integer
+    # change_column :list_pengeluarans, :jumlah, :integer
+    change_column :list_pengeluarans, :jumlah, 'integer USING CAST(column_name AS integer)'
   end
 end
